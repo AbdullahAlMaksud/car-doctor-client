7494115a61910c3f186ef0899d83a18b86cd25c3
AbdullahAlMaksud <maksud.workspace@gmail.com>
2024-05-05 00:37:18 +0600

    Module 58 Complete

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,29 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main";
+import Home from "../pages/Home/Home/Home";
+import Login from "../pages/Login/Login";
+import Signup from "../pages/Signup/Signup";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Main/>,
+        children:[
+            {
+                path: '/',
+                element:<Home/>
+
+            },
+            {
+                path: '/login',
+                element: <Login/>
+            },
+            {
+                path: '/signup',
+                element: <Signup/>
+            }
+        ]
+    },
+]);
+
+export default router;
