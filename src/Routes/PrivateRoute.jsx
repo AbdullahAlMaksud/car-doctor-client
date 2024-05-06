690f6728e7b018bb1bd94a4b6fabbabe56a6df4b
AbdullahAlMaksud <maksud.workspace@gmail.com>
2024-05-07 00:43:05 +0600

    JWT first token create and got it from server and client side cookies

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -1,9 +1,12 @@
-import React, { useContext } from 'react';
+import { useContext } from 'react';
 import { AuthContext } from '../Provider/AuthProvider';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 
 const PrivateRoute = ({children}) => {
     const {user, loading} = useContext(AuthContext);
+
+    const location = useLocation()
+    // console.log(location.pathname)
 
     if(user?.email){
         return children;
@@ -11,7 +14,7 @@ const PrivateRoute = ({children}) => {
     if(loading){
         return <div className='py-10 flex justify-center items-center'><span className='loading loading-ring loading-lg'></span></div>
     }
-    return <Navigate to={'/login'} replace/>
+    return <Navigate state={location.pathname} to='/login' replace/>
 };
 
 export default PrivateRoute;
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,24 +1,36 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import img from '../../assets/images/login/login.svg'
-import { useContext, useEffect } from 'react';
+import { useContext } from 'react';
 import { AuthContext } from '../../Provider/AuthProvider';
+import axios from 'axios';
 const Login = () => {
 
-    const {signIn} = useContext(AuthContext);
+    const { signIn } = useContext(AuthContext);
+    const location = useLocation();
+    console.log(location)
+    const navigate = useNavigate()
 
-    const handleLogin = event =>{
+    const handleLogin = event => {
         event.preventDefault();
         const form = event.target;
         const email = form.email.value;
         const password = form.password.value;
-        console.log({email, password})
 
         signIn(email, password)
-        .then(result=>{
-            const user = result.user;
-            console.log(user)
-        })
-        .catch(error=>console.log(error))
+            .then(result => {
+                const loggedInUser = result.user;
+                console.log(loggedInUser)
+                const user = { email }
+                //get access token
+                axios.post(`http://localhost:5000/jwt`, user, { withCredentials: true })
+                    .then(res => {
+                        if (res.data.success) {
+                            console.log(res.data)
+                            navigate(location?.state ? location?.state : '/')
+                        }
+                    })
+            })
+            .catch(error => console.log(error))
     }
     return (
         <div className="hero min-h-screen bg-base-200">
